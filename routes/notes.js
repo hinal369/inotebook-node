94cf9b08d54ccca0c49a677a707231bb65f2dcd4
hinal369 <khalasihinal1998@gmail.com>
2026-07-10 18:08:11 +0530

    Get all notes API

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const authentication = require("../middleware/authentication");
+const Notes = require('../models/Notes');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/fetchallnotes', authentication, async (req, res) => {
+  try {
+    const { id } = req.user;
+    const notes = await Notes.find({ user: id });
+    res.json({ notes })
+  } catch (error) {
+    res.status(500).send("Something is wrong!");
+  }
 });
 
 
